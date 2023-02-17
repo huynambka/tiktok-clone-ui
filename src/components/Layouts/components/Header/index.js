7fb387cb4acf9140cb5b6a46b420574b3dc23c49
huynambka <109231260+huynambka@users.noreply.github.com>
2023-02-17 21:42:11 +0700

    header #1

--- src/components/Layouts/components/Header/index.js
+++ src/components/Layouts/components/Header/index.js
@@ -1,5 +1,8 @@
 import classNames from 'classnames/bind';
 import styles from './Header.module.scss';
+import images from '~/assets/images';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCircleXmark, faMagnifyingGlass, faSpinner } from '@fortawesome//free-solid-svg-icons';
 
 // Init classnames
 const cx = classNames.bind(styles);
@@ -8,8 +11,20 @@ function Header() {
     return (
         <header className={styles.wrapper}>
             <div className={cx('inner')}>
-                {/* Logo */}
-                {/* Search */}
+                <div className={cx('logo')}>
+                    <img src={images.logo} alt="TikTok" />
+                </div>
+                <div className={cx('search')}>
+                    <input placeholder="Search accounts and videos" spellCheck={false} />
+                    <button className={cx('clear')}>
+                        <FontAwesomeIcon icon={faCircleXmark} />
+                    </button>
+                    <FontAwesomeIcon className={cx('loading')} icon={faSpinner} />
+                    <button className={cx('search-btn')}>
+                        <FontAwesomeIcon icon={faMagnifyingGlass} />
+                    </button>
+                </div>
+                <div className={cx('actions')}></div>
             </div>
         </header>
     );
